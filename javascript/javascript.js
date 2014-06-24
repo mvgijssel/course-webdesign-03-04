463ad23c5ee26d4e3fbfad6f31f6fa1c5a5d570f
Maarten van Gijssel <maarten@vgijssel.nl>
2014-06-25 01:14:06 +0200

    Added a validate form method

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,17 @@
 function swapStyleSheet(sheet) {
     document.getElementById("page-style").setAttribute("href", "stylesheets/" + sheet + ".css");
 }
+
+function validateForm() {
+    var x = document.forms["login"]["email"].value;
+    if (x==null || x=="") {
+        alert("The email must be filled out");
+        return false;
+    }
+
+    x = document.forms["login"]["password"].value;
+    if (x==null || x=="") {
+        alert("The password must be filled out");
+        return false;
+    }
+}

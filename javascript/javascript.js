4a03663081794d535dd8c8c88275278521969d20
Maarten van Gijssel <maarten@vgijssel.nl>
2014-06-25 01:24:59 +0200

    Created a setDate function

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -15,3 +15,23 @@ function validateForm() {
         return false;
     }
 }
+
+function setDate(id) {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd
+    }
+
+    if(mm<10) {
+        mm='0'+mm
+    }
+
+    today = dd+'/'+mm+'/'+yyyy;
+
+    el = document.getElementById(id);
+    el.innerHTML = today;
+}
